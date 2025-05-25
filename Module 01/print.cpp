b1dff825bcaef4278b9ee89f2d8b744f53133cec
sabilar-rahman <sabilarrahman36@gmail.com>
2025-05-26 01:07:39 +0600

    Implement code changes to enhance functionality and improve performance

---
+++ Module 01/print.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main()
+{
+    int x = 10;
+    char c = 'A';
+    double d = 54.54;
+    std ::cout << x << " " << c << std::endl
+               << d << std::endl;
+    std ::cout << " My number is:" << x << std::endl;
+    return 0;
+}
