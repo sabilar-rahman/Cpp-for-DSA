6da36e3b78dcd8b630d710f6b2c84fd2a502f3bf
sabilar-rahman <sabilarrahman36@gmail.com>
2025-05-26 01:53:57 +0600

    Setprecision - to  show after coma

---
+++ Module 01/Setprecision.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    double d = 23.358;
+    cout << d << endl;
+    cout << setprecision(3) << d << endl;
+    return 0;
+}
---
+++ Module 01/efo.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x = 10;
+   while (cin >>x)
+   {
+       cout << x << endl;
+   }
+   
+}
