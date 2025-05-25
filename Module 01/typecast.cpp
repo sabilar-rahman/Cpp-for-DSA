de2f1473c29d52630c78ba67ddeef651df60bc0b
sabilar-rahman <sabilarrahman36@gmail.com>
2025-05-26 01:46:03 +0600

    Typecasting

--- Module 01/typecast.cpp
+++ Module 01/typecast.cpp
@@ -1,13 +1,12 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
-int main (){
+int main()
+{
     int x;
     cin >> x;
     cout << x << endl;
     int y = 'a';
     cout << (int)y << endl;
     return 0;
-
 }
