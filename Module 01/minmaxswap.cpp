ba48b51a0609804a1b2a20c9d230da72b41cfd86
sabilar-rahman <sabilarrahman36@gmail.com>
2025-05-26 10:03:57 +0600

    Add initial implementation of a temporary variable swap function

---
+++ Module 01/minmaxswap.cpp
@@ -0,0 +1,36 @@
+// #include <iostream>
+// #include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // int x, y;
+    // cin >> x >> y;
+    // if (x < y)
+    // {
+    //     cout << x << endl;
+    // }
+    // else
+    // {
+    //     cout << y << endl;
+    // }
+
+    // cout << min(x, y) << endl;
+
+    //---------- swap ----------
+
+    int a, b;
+
+    cin >> a >> b;
+
+    // int temp = a;
+    // a = b;
+    // b = temp;
+
+    swap(a, b);
+
+    cout << a << " " << b << endl;
+
+    return 0;
+}
---
+++ Module 01/switchCase.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    char x;
+    cin >> x;
+    switch (x)
+    {
+    case 'a':
+        cout << "vowel" << endl;
+        break;
+    case 'e':
+        cout << "vowel" << endl;
+        break;
+    case 'i':
+        cout << "vowel" << endl;
+        break;
+    case 'o':
+        cout << "vowel" << endl;
+        break;
+    case 'u':
+        cout << "vowel" << endl;
+        break;
+    default:
+        cout << "Default" << endl;
+        break;
+    }
+    return 0;
+}
