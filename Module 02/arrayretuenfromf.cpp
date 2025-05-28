06c277cd5b62f53ee3d609543d7712f01397688d
sabilar-rahman <sabilarrahman36@gmail.com>
2025-05-28 20:22:25 +0600

    Max Solution

---
+++ Module 02/arrayretuenfromf.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int *fun()
+{
+    // int a[5];
+    int *a = new int[5]; // that will store into heap memory
+}
+int main()
+{
+
+    int *a = fun();
+}
