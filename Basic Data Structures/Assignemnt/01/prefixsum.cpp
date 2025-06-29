57a42efa9abaf03722d2a421cda562aed67fd17c
sabilar-rahman <sabilarrahman36@gmail.com>
2025-06-30 01:28:37 +0600

    Refactor prefix sum implementation to use vector<long long> and streamline output in reverse order

--- Basic Data Structures/Assignemnt/01/prefixsum.cpp
+++ Basic Data Structures/Assignemnt/01/prefixsum.cpp
@@ -19,7 +19,7 @@
 // 2 4 1 5 3
 // Sample Output 0
 
-// 15 12 7 6 2 
+// 15 12 7 6 2
 // Explanation 0
 
 // The prefix sum of the given array is: 2 6 7 12 15.
@@ -30,28 +30,123 @@
 // 1000000000 1000000000 1000000000
 // Sample Output 1
 
-// 3000000000 2000000000 1000000000 
+// 3000000000 2000000000 1000000000
 
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+
+//     int n;
+//     cin >> n;
+//     int array[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> array[i];
+//     }
+//     int sum = 0;
+//     for (int i = n - 1; i >= 0; i--)
+//     {
+//         sum += array[i];
+//         cout << sum << " ";
+//     }
+//      // print in reverse order
+//     for (int i = n - 1; i >= 0; i--) {
+//         cout << array[i] << " ";
+//     }
+
+//     cout << endl;
+
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+
+//     int n;
+//     cin >> n;
+//     int array[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> array[i];
+//     }
+
+//     int sum = 0;
+//     for (int i = n - 1; i >= 0; i--)
+//     {
+//         sum += array[i];
+//         array[i] = sum;
+//     }
+
+//     // print in reverse order
+//     for (int i = n - 1; i >= 0; i--) {
+//         cout << array[i] << " ";
+//     }
+
+//     cout << endl;
+
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     // vector<int> a(n);
+//     vector<long long> a(n);
+//     for (int i = 0; i < n; ++i)
+//     {
+//         cin >> a[i];
+//     }
+
+//     // Calculate the prefix sum array.
+//     // vector<int> presum(n);
+//     vector<long long> presum(n);
+//     presum[0] = a[0];
+//     for (int i = 1; i < n; ++i)
+//     {
+//         presum[i] = presum[i - 1] + a[i];
+//     }
+
+//     // Print in reverse order.
+//     for (int i = n - 1; i >= 0; --i)
+//     {
+//         cout << presum[i] << " ";
+//     }
+//     return 0;
+// }
 
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-
     int n;
     cin >> n;
-    int array[n];
-    for (int i = 0; i < n; i++)
+    // vector<int> a(n);
+    vector<long long> array(n);
+    for (int i = 0; i < n; ++i)
     {
         cin >> array[i];
     }
-    int sum = 0;
-    for (int i = n - 1; i >= 0; i--)
+    // Calculate the prefix sum array.
+    // vector<int> presum(n);
+    vector<long long> presum(n);
+    presum[0] = array[0];
+    for (int i = 1; i < n; ++i)
     {
-        sum += array[i];
-        cout << sum << " ";
+        presum[i] = presum[i - 1] + array[i];
     }
 
-    
+    // Print in reverse order.
+    for (int i = n - 1; i >= 0; --i)
+    {
+        cout << presum[i] << " ";
+    }
     return 0;
 }
