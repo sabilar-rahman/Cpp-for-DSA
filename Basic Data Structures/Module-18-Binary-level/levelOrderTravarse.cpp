12cd707b47c7c2d0406de4d51be343baca85764e
sabilar-rahman <sabilarrahman36@gmail.com>
2025-08-11 00:07:32 +0600

    Level order traverse

---
+++ Basic Data Structures/Module-18-Binary-level/levelOrderTravarse.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void levelOrder(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    while (q.empty() == false)
+    {
+        // ber kore ana
+        Node *f = q.front();
+        q.pop();
+
+        // oi node niye kaj kra
+
+        cout << f->val << " ";
+
+        //  childersn niye kaj kra
+        if (f->left)
+        {
+            q.push(f->left);
+        }
+        if (f->right)
+        {
+            q.push(f->right);
+        }
+    }
+}
+
+int main()
+{
+    Node *root = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(60);
+
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    b->left = d;
+    b->right = e;
+
+    levelOrder(root);
+
+    return 0;
+}
