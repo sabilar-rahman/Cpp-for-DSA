aebfb9f3cc5c8296e4c95ba7ddce676008888b6d
sabilar-rahman <sabilarrahman36@gmail.com>
2025-08-30 13:59:19 +0600

    can go grid

---
+++ Algorithm/Assignment/01/CanGoGrid.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char gird[1005][1005];
+bool visited[1005][1005];
+vector<pair<int, int>> moved = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+int n, m;
+bool valid(int i, int j)
+{
+    if (i < 0 || j < 0 || i >= n || j >= m)
+        return false;
+    return true;
+}
+void dfs(int si, int sj)
+{
+    // cout << si << " " << sj << endl;
+    visited[si][sj] = true;
+    for (int i = 0; i < 4; i++)
+    {
+        int ci, cj;
+        ci = si + moved[i].first;
+        cj = sj + moved[i].second;
+        if (valid(ci, cj) == true && visited[ci][cj] == false && gird[ci][cj] != '#')
+            // {
+            //     continue;
+            // }
+            // pair<int, int> child = {ci, cj};
+            dfs(ci, cj);
+    }
+}
+int main()
+{
+
+    //  int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> gird[i][j];
+        }
+    }
+
+    int si, sj, ei, ej;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (gird[i][j] == 'A')
+            {
+                si = i;
+                sj = j;
+            }
+            if (gird[i][j] == 'B')
+            {
+                ei = i;
+                ej = j;
+            }
+        }
+    }
+
+    // int si, sj;
+    // cin >> si >> sj;
+
+    memset(visited, false, sizeof(visited));
+
+    dfs(si, sj);
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //     {
+    //         cout << visited[i][j];
+    //     }
+    //     cout << endl;
+    // }
+
+    if (visited[ei][ej])
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+    return 0;
+}
